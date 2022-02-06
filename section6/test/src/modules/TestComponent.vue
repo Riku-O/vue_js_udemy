8cdd0dea77051059a222136c003558584e44c0d3
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2022-02-06 18:37:36 +0900

    SFC

---
+++ section6/test/src/modules/TestComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>テストです {{ testData }}</div>
+</template>
+
+<script>
+export default {
+  name: "TestComponent",
+  data() {
+    return {
+      testData: "テストdataです",
+    };
+  },
+};
+</script>
+
+<style></style>
