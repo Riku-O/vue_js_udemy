20b84102c003756a342c42f8cd478fc5909f2d88
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2022-02-20 18:30:16 +0900

    動的パラメータ +props 補足

--- section7/vuerouter/src/components/BookDetail.vue
+++ section7/vuerouter/src/components/BookDetail.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <p>本の詳細</p>
-    <p>タイトル： {{ title }}</p>
-    <p>本の内容： {{ content }}</p>
+    <p>タイトル： {{ books[bookIndex].title }}</p>
+    <p>本の内容： {{ books[bookIndex].content }}</p>
   </div>
 </template>
 
@@ -12,6 +12,21 @@ export default {
     id: Number,
     title: String,
     content: String,
+  },
+  data() {
+    return {
+      bookIndex: this.$route.params.id - 1,
+      books: [
+        { id: 1, title: "タイトル1", content: "本の内容1" },
+        { id: 2, title: "タイトル2", content: "本の内容2" },
+        { id: 3, title: "タイトル3", content: "本の内容3" },
+      ],
+    };
+  },
+  created() {
+    if (this.$route.params.id > this.books.length) {
+      this.$router.push("/book");
+    }
   },
 };
 </script>
