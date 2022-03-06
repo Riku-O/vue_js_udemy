48d7e003726751eb4e67d4e4e694933696d563f8
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2022-03-06 17:19:51 +0900

    vuex stateとmutations

--- section9/vuex/src/store/index.js
+++ section9/vuex/src/store/index.js
@@ -4,8 +4,15 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    // 初期値
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
   actions: {},
   getters: {},
   modules: {},
