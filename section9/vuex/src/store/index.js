714b459ac2c3939733c63115217c6e1c85598d97
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2022-03-06 18:36:34 +0900

    actions->mutations->state

--- section9/vuex/src/store/index.js
+++ section9/vuex/src/store/index.js
@@ -17,7 +17,17 @@ export default new Vuex.Store({
       state.count += payload.value;
     },
   },
-  actions: {},
+  actions: {
+    // incrementAction(context) {
+    //   context.commit("increment");
+    // },
+    incrementAction({ commit }) {
+      commit("increment");
+    },
+    addCountAction({ commit }, payload) {
+      commit("addCount", payload);
+    },
+  },
   getters: {},
   modules: {},
 });
